ab366ccdb357b63beb86163164f2db8fbb23fe0f
David Borges <davidborges@protonmail.com>
2020-10-18 10:25:28 -0300

    Tratamento de erro de desconexão

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 import enum
 import asyncio
 
+import websockets
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -130,22 +131,25 @@ async def search(symbol: str):
 
 
 class ConnectionManager:
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
+	def __init__(self):
+		self.active_connections: List[WebSocket] = []
 
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
+	async def connect(self, websocket: WebSocket):
+		await websocket.accept()
+		self.active_connections.append(websocket)
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+	def disconnect(self, websocket: WebSocket):
+		self.active_connections.remove(websocket)
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+	async def send_personal_message(self, message: str, websocket: WebSocket):
+		try:
+			await websocket.send_text(message)
+		except websockets.exceptions.ConnectionClosedError as e:
+			pass
 
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
+	async def broadcast(self, message: str):
+		for connection in self.active_connections:
+			await connection.send_text(message)
 
 manager = ConnectionManager()
 
